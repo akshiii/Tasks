5bb5f5023d5f74a7da24b30e645ce37e16971253
Askhita <aksitatyagi1001@gmail.com>
2018-10-24 12:52:56 +0530

    Adding BST program 1

---
+++ src/java/BST/BST.java
@@ -0,0 +1,27 @@
+package task3;
+
+public class BST {
+	public Node root;
+	
+	public BST() {
+		
+		root= null;
+	}
+	public int visit() {
+		return root.getData();
+	}
+	public void add(int val) {
+		root = add(root,val);
+	}
+	public Node add(Node root, int val) {
+		if (root == null)
+			root= new Node(val);
+		else if (root.getData()>val)
+			root.left = add(root.left,val);
+		else if (root.getData()<val)
+			root.right = add(root.right,val);
+		return root;
+		
+	}
+	
+}
---
+++ src/java/BST/Binarytree.java
@@ -0,0 +1,57 @@
+package task3;
+
+import java.util.Scanner;
+
+class Binarytree { 
+	public static void inOrder(Node root) {
+		if(root==null)
+			return;
+		inOrder(root.left);
+		System.out.print(root.getData()+" ");
+		inOrder(root.right);
+	}
+	public static void preOrder(Node root) {
+		if(root==null)
+			return;
+		System.out.print(root.getData()+" ");
+		inOrder(root.left);
+		inOrder(root.right);
+	}
+	public static void postOrder(Node root) {
+		if(root==null)
+			return;
+		inOrder(root.left);
+		inOrder(root.right);
+		System.out.print(root.getData()+" ");
+	}
+	
+ public static void main(String[] args) { 
+    BST bst = new BST();
+    /*bst.add(10);
+    bst.add(5);
+    bst.add(1);
+    bst.add(7);
+    bst.add(40);
+    bst.add(50);
+    */
+    int option=1;
+    Scanner sc= new Scanner(System.in);
+    //System.out.println("Enter one value for tree:");
+    int value;
+    
+    while(option!=0 ){
+        System.out.print("Enter the value:");
+    	value= sc.nextInt();
+    	bst.add(value);
+    	System.out.print("Want to enter more values? Enter 1 for YES and 0 for NO");
+    	option=sc.nextInt();
+    }
+    System.out.println("Inorder for the binary tree is::");
+    inOrder(bst.root);
+    System.out.println("Preorder for the binary tree is::");
+    preOrder(bst.root);
+    System.out.println("Postorder for the binary tree is::");
+    postOrder(bst.root);
+ } 
+} 
+
