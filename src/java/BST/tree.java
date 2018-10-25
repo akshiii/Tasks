3b92c4d13dca6e41849755af44e7a705798904b0
Askhita <aksitatyagi1001@gmail.com>
2018-10-25 14:57:06 +0530

    Adding program 2 of bst

---
+++ src/java/BST/tree.java
@@ -0,0 +1,27 @@
+package task3;
+
+public class tree {
+	public Node root;
+	
+	public tree() {
+		
+		root= null;
+	}
+	public int visit() {
+		return root.getData();
+	}
+	public void add(int val) {
+		root = add(root,val);
+	}
+	public Node add(Node root, int val) {
+		if (root == null)
+			root= new Node(val);
+		else {
+			root.left = add(root.left,val);
+			root.right = add(root.right,val);
+			}
+		return root;
+		
+	}
+	
+}
