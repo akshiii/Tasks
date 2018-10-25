0695da9933ce36bb2dfb9f08341f3a1d71f146b5
Askhita <aksitatyagi1001@gmail.com>
2018-10-25 15:15:31 +0530

    bst programs..

---
+++ src/java/BST/Node.java
@@ -0,0 +1,24 @@
+//package task3;
+
+public class Node {
+	public Node left,right;
+	 int data;
+	 
+	 public Node() {
+		 left= null;
+		 right=null;
+		 data =0;
+		
+	 }
+	 public Node(int val) {
+		 left=right=null;
+		 this.data = val;
+	 }
+	 public void setData(int val) {
+		 this.data= val;
+	 }
+	 public int getData() {
+		 return data;
+	 }
+}
+
